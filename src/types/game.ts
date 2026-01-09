c06e4bda6812890f9abbc0746065cfa2412af254
kawal <kawaldhillon5@gmail.com>
2026-01-09 14:45:29 -0500

    added interface for validateMove and makeMove funcs

--- src/types/game.ts
+++ src/types/game.ts
@@ -6,3 +6,14 @@ export interface GameStatus {
     winner: Player | 'Draw' | null;
     gameOver: boolean;
 }
+
+export interface ValidateMoveResult {
+    isValid: boolean;
+    error: string|null
+}
+
+export interface MoveResult {
+    board: Board;
+    success: boolean;
+    error: string | null;
+}
