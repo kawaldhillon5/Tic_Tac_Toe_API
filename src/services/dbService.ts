69a7821c90634cdb18923f4cb002d462bd81ba73
Kawal Dhillon <kawaldhillon5@gmail.com>
2026-01-11 21:07:12 -0500

    created updateGame and getGameById funcs

--- src/services/dbService.ts
+++ src/services/dbService.ts
@@ -1,7 +1,10 @@
 import pool from '../config/db.js';
-import type { Board } from '../types/game.js';
+import type { Board, GameRow } from '../types/game.js';
 
 export const createGame = async (): Promise<string> => {
+
+    const date  = new Date()
+
     const board: Board = [
         [null, null, null],
         [null, null, null],
@@ -9,9 +12,45 @@ export const createGame = async (): Promise<string> => {
     ];
 
 
-    const queryText = 'INSERT INTO games (board) VALUES ($1) RETURNING id';
+    const queryText = 'INSERT INTO games (board, created_at) VALUES ($1, $2) RETURNING id';
     
     const res = await pool.query(queryText, [JSON.stringify(board)]);
 
     return res.rows[0].id;
 };
+
+export const updateGame = async (game: GameRow): Promise<GameRow> =>{
+    const queryText = `
+        UPDATE games 
+        SET board = $1, current_turn = $2, status = $3, winner = $4 
+        WHERE id = $5 
+        RETURNING *;
+    `;
+    
+    const values = [
+        JSON.stringify(game.board), 
+        game.current_turn, 
+        game.status, 
+        game.winner, 
+        game.id
+    ];
+
+    const res = await pool.query(queryText, values);
+    
+    if (res.rows.length === 0) {
+        throw new Error('Could not find game to update');
+    }
+
+    return res.rows[0]; 
+}
+
+export const getGameById =  async (id: string): Promise<GameRow | null> =>{
+    const queryText  = 'SELECT * FROM games WHERE id = $1';
+    const res = await pool.query(queryText, [id])
+
+    if (res.rows.length === 0) {
+        return null; 
+    }
+
+    return res.rows[0];
+} 
