51ae0ebe63ca81b2655ade971fe29959b4a0d457
Kawal Dhillon <kawaldhillon5@gmail.com>
2026-01-11 19:09:38 -0500

    created createGame handler and endpoint

--- src/controllers/gameController.ts
+++ src/controllers/gameController.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import type { Request, Response } from 'express';
 import * as GameService from '../services/gameService.js';
+import * as DbService from '../services/dbService.js';
+
 
 export const handleMove = (req: Request, res: Response) => {
     const { board, row, column, player } = req.body;
@@ -22,3 +24,20 @@ export const handleMove = (req: Request, res: Response) => {
         status: gameStatus
     });
 };
+
+export const handleCreateGame = async (req: Request, resp: Response) =>{
+    try {
+        const gameId = await DbService.createGame();
+    
+        resp.status(201).json({
+            success:true,
+            gameId: gameId
+        });
+    } catch (err){
+        console.error("Error Creating game: ", err);
+        resp.status(500).json({
+            success: false,
+            message: "Failed to initilize new game"
+        });
+    }
+}
--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -4,5 +4,6 @@ import * as gameController from '../controllers/gameController.js';
 const router = Router();
 
 router.post('/move', gameController.handleMove);
+router.post('/create-game',gameController.handleCreateGame)
 
 export default router;
