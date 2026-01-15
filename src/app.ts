6733d34c25d3ee70f11fd197bdd95bb2fa988405
kawal <kawaldhillon5@gmail.com>
2026-01-15 09:11:42 -0500

    added websocket server to httpserver

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express, {type Application} from 'express';
 import { createServer } from 'http';
-import { Server } from 'socket.io';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
 import gameRoutes from './routes/gameRoutes.js'
+import { initSocket } from './services/socketService.js';
 
 dotenv.config();
 
@@ -12,33 +12,17 @@ dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
-const ORIGIN = process.env.ORIGIN
+const ORIGIN = process.env.ORIGIN || ""
 
 const httpServer  = createServer(app)
-const io = new Server(httpServer,{
-  cors: {
-    origin: ORIGIN, 
-    methods: ["GET", "POST"]
-  }
-})
+
 
 app.use(cors({
   origin: ORIGIN
 }));
 app.use(express.json());
+initSocket(httpServer,ORIGIN)
 
-io.on('connection', (socket) => {
-  console.log('user connected:', socket.id);
-
-  socket.on('join-game', (gameId: string) => {
-    socket.join(gameId);
-    console.log(`User ${socket.id} joined room: ${gameId}`);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
 
 app.use('/api/game', gameRoutes);
 
@@ -46,4 +30,3 @@ httpServer.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-export { io };
