f445824e8427a775831b817621c58fb3514928d7
kawal <kawaldhillon5@gmail.com>
2026-01-15 23:43:51 -0500

    added assigned_name listner to ws

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,5 +1,6 @@
 import { Server as HttpServer } from 'http';
 import { Server, Socket } from 'socket.io';
+import { GetUsername } from '../utils/usernameGenerator.js';
 
 let io: Server;
 
@@ -12,11 +13,23 @@ export const initSocket = (httpServer: HttpServer, origin: string) => {
   });
 
   io.on('connection', (socket: Socket) => {
+
+    
     console.log('User connected:', socket.id);
+
+    socket.on("assigned_username",(name:string)=>{
+      if(name.length>0){
+        socket.data.username = name
+      } else {
+        const username = GetUsername();
+        socket.emit("assigned_name", username);
+      }
+    })
+
 
     socket.on('join-game', (gameId: string) => {
       socket.join(gameId);
-      console.log(`User ${socket.id} joined room: ${gameId}`);
+      console.log(`User ${socket.data.username} joined room: ${gameId}`);
     });
 
     socket.on('disconnect', () => {
